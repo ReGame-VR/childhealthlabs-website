378e6f425fc11a301725b9abdc632e5112ce8591
Ryan Bradford <ryanbradford333@gmail.com>
2019-03-27 19:55:21 -0400

    Updated logo + location + removed PP

--- js/inject_footer.js
+++ js/inject_footer.js
@@ -6,7 +6,7 @@ document.getElementById("footer").innerHTML = `
                 <div class="col_wrap">
                     <div class="col">
                         <h3>Our address</h3>
-                        <address class="map">30 Leon St, Boston, MA 02115</address>
+                        <address class="map">360 Huntington Ave, Boston, MA 02115</address>
                         <address class="mail"><a href="mailto:regamevrlab@neu.edu">regamevrlab@neu.edu</a></address>
                         <address class="phone"><a href="tel:617-373-6107">617-373-6107</a></address>
                     </div>
